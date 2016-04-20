266713da7683639cd84cc62fc7c5935fa133cb98
Vincent Gire <vincent.gire@gmail.com>
2016-04-20 09:41:22 +0200

    Fixed order

--- publish.sh
+++ publish.sh
@@ -5,6 +5,7 @@ git clone -b gh-pages https://github.com/calldesk/booking-client-api.git
 cd booking-client-api
 rm -R *
 cp -R ../doc/* .
+git config user.email "vincent.gire@gmail.com"
 git config user.name "Vincent Gire"
 git commit -a -m "Auto publish"
 git push origin gh-pages
