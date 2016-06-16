5ac6c22d0ef36b3c773c08eb18525f1bffd82df9
nicolaspanel <nicolas.panel@live.fr>
2016-06-16 18:24:52 +0200

    include uploadS3 script

---
+++ scripts/uploadS3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Pack module for production and upload to s3 with current version
+
+# To test manually
+# babel -d softphone/lib/ softphone/src/ && rm softphone.zip && zip -r softphone.zip softphone
+
+# http://stackoverflow.com/a/246128
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_PATH=`pwd`
+NAME=`node -p -e "require('$SCRIPT_PATH/../package.json').name"`
+cd $SCRIPT_PATH
+./packS3.sh "$NAME-prod/src/*" "$NAME-prod/test/*" "$NAME-prod/scripts/*" "$NAME-prod/.*"
+cd $CURRENT_PATH
