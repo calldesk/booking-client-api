b6b631bce1e3af6b837a002aa7f017c770c32641
nicolaspanel <nicolas.panel@live.fr>
2016-09-01 18:05:58 +0200

    Updates

--- api_project.js
+++ api_project.js
@@ -1,1 +1,27 @@
-define({  "name": "CallDesk booking client API",  "version": "0.1.0",  "description": "This is the API client should provide CallDesk to activate the CallDesk booking service.",  "title": "CallDesk booking client API",  "url": "http://api.client.com/v1",  "order": [    "Ressource",    "Slot",    "GetSlot",    "BookSlot",    "Call"  ],  "template": {    "forceLanguage": "en",    "withCompare": false,    "withGenerator": false  },  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-09-01T14:21:45.773Z",    "url": "http://apidocjs.com",    "version": "0.15.1"  }});
+define({
+  "name": "CallDesk booking client API",
+  "version": "0.1.0",
+  "description": "This is the API client should provide CallDesk to activate the CallDesk booking service.",
+  "title": "CallDesk booking client API",
+  "url": "http://api.client.com/v1",
+  "order": [
+    "Ressource",
+    "Slot",
+    "GetSlot",
+    "BookSlot",
+    "Call"
+  ],
+  "template": {
+    "forceLanguage": "en",
+    "withCompare": false,
+    "withGenerator": false
+  },
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-09-01T16:05:45.110Z",
+    "url": "http://apidocjs.com",
+    "version": "0.15.1"
+  }
+});
